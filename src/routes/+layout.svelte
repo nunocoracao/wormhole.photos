8d56e67644043d9892d13b81660b8fc3efd42cec
Nuno Coração <nunocoracao@gmail.com>
2023-10-05 22:27:01 +0100

    initial dev test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,37 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Wormhole</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.instagram.com/wormhole_photos/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Instagram
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/wormholephotos"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/nunocoracao/wormhole.photos"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
