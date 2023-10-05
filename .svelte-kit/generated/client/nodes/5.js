76abf17b376c7adf04d6ebff8498945f085b0236
Nuno Coração <nunocoracao@gmail.com>
2023-10-05 22:12:42 +0100

    initial image gallery

---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/post/[slug]/+page.svelte";
---
+++ src/lib/feed.svelte
@@ -0,0 +1,48 @@
+
+<section class="grid grid-cols-2 md:grid-cols-4 gap-2">
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1597077962467-be16edcc6a43?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2MzZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1591775161903-497839a443c1?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3MzR8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+	</div>
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296539691-67feaadf389e?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1542835435-4fa357baa00b?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NzN8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1553184570-557b84a3a308?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NTF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+	</div>
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1603855873822-0931a843ee3a?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3ODJ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1509130446053-899ae7358ce6?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1508931133503-b1944a4ecdd5?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3OTV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+	</div>
+	<div class="grid gap-4">
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1633053663400-655b31fb88ac?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MDV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1578170222009-c7893aa20afd?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MTZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+		<div>
+			<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1610220941077-1ec123e7c043?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4NjV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
+		</div>
+	</div>
+
+</section>
