e239bb4e2a08eaa150922409d0b2424e654e9ca5
Nuno Coração <nunocoracao@gmail.com>
2023-10-22 12:14:48 +0100

    implemented native infinite scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from "svelte";
     import { ProgressRadial } from "@skeletonlabs/skeleton";
-    import { inview } from "svelte-inview";
+    //import { inview } from "svelte-inview";
     import { getFeed } from "$lib/firebase.js";
     import Feed from "../components/Feed.svelte";
     import { items } from "$lib/stores.js";
@@ -9,7 +9,7 @@
     let items_value;
     let element;
     let loading = true;
-    let limit = 20;
+    let limit = 10;
     let startAt = 0;
     let hasMore = true;
 
@@ -24,14 +24,22 @@
             startAt = data[data.length - 1].timestamp;
             if (data.length < limit) hasMore = false;
             var tempItems = items_value;
-            for(var i in data)
-                tempItems.push(data[i]);
+            for (var i in data) tempItems.push(data[i]);
             items.set(tempItems);
         });
     };
 
     onMount(() => {
         fetchFeed();
+        var observer = new IntersectionObserver(
+            function (entries) {
+                if (entries[0].isIntersecting === true)
+                    handleChange()
+            },
+            { threshold: [1] }
+        );
+        observer.observe(element);
+
     });
 
     const handleChange = (e) => {
@@ -46,7 +54,7 @@
     <div class="max-w-[1500px] mx-auto pt-10 overflow-hidden">
         <Feed items={items_value} />
     </div>
-    <div use:inview={{}} on:change={handleChange} />
+    <div bind:this={element} />
     <div class="flex justify-center mt-[100px] mb-[100px]">
         {#if loading}
             <ProgressRadial value={undefined} />
